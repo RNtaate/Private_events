cf8845c47dc28b9f8d3a5daeeefbd57cddebf047
RNtaate <royhyde14@gmail.com>
2020-09-24 18:08:13 +0300

    Add index, new, show, create, event_params methods to events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,24 @@
 class EventsController < ApplicationController
   def index
-    @events = Event.pluck(:description)
+    @events = Event.all
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.create(event_params)
+    redirect_to event_path(@event.id)
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:description, :date)
   end
 end
