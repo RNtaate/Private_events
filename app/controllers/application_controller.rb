e7b9be117f20b1b029de0f39f0b9871b049e90fc
RNtaate <royhyde14@gmail.com>
2020-09-24 18:09:40 +0300

    Add current_user, signed_in?  methods to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+  def current_user
+    User.find_by(id: session[:current_user_id])
+  end
+
+  helper_method :signed_in?
+  def signed_in?
+    !current_user.nil?
+  end
 end
