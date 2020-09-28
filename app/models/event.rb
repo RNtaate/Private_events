ba9f46a0b2985445d30eba9ae28a04a6ea128c91
RNtaate <royhyde14@gmail.com>
2020-09-28 16:38:31 +0300

    Create associations amongst event_attendance, event, user models

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,6 @@
 class Event < ApplicationRecord
+  has_many :event_attendances, foreign_key: :attended_event_id
+  has_many :attendees, through: :event_attendances
+
   belongs_to :creator, class_name: 'User'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   validates :name, presence: true, uniqueness: true
 
   has_many :created_events, foreign_key: :creator_id, class_name: 'Event'
+
+  has_many :event_attendances, foreign_key: :attendee_id
+  has_many :attended_events, through: :event_attendances
 end
