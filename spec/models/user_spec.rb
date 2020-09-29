fe43644f0444322afce7b1411de0dc787a32ebe6
RNtaate <royhyde14@gmail.com>
2020-09-29 15:14:10 +0300

    Add testing for presence, uniqueness validations for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+
+  context "Validates user's name presence" do
+    it "Should be valid" do
+      @user = User.create(name: "Rahan")
+      expect(@user.save).to be true
+    end
+
+    it "Should NOT be valid" do
+      @user = User.create(name: "")
+      expect(@user.save).not_to be true
+    end
+  end
+
+  context "Validates user's name uniqueness" do
+    let!(:user1){User.create(name: "Rayhan")}
+    it "Should be valid" do
+      user2 = User.create(name: "Ray")
+      expect(user2.save).to be true
+    end
+
+    it "Should be invalid" do
+      user2 = User.create(name: "Rayhan")
+      expect(user2.save).not_to be true
+    end
+  end
 end
