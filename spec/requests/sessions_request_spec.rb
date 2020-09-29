837fc89d5d2e9acfeb78ee0eebdfbefb55ee1e10
RNtaate <royhyde14@gmail.com>
2020-09-29 19:02:36 +0300

    Add request testing for create method in sessions

--- spec/requests/sessions_request_spec.rb
+++ spec/requests/sessions_request_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "Sessions", type: :request do
-
+  describe 'POST create' do
+    it "Should save current_user_id inside session hash" do
+      user1 = User.create(name: "Rayhan")
+      post "/sessions", :params => {name: "Rayhan"}
+      expect(session[:current_user_id]).to eq(user1.id)
+    end
+  end
 end
