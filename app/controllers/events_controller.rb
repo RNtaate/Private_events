04ee105e75f8df5a24165db99b71c339e2f71615
RNtaate <royhyde14@gmail.com>
2020-09-29 13:40:22 +0300

    Update create method to register invited guests.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -14,7 +14,16 @@ class EventsController < ApplicationController
   def create
     if signed_in?
       @event = current_user.created_events.build(event_params)
+      @attendee = params[:event_attendance][:attendee_id]
       @event.save
+
+      @attendee.each do |x|
+        if !(x.empty?)
+          @user = User.find(x)
+          @event.attendees << @user
+        end
+      end
+
       redirect_to root_path
     end
   end
