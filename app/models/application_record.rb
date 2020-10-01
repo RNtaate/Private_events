89d5d89a7b0ddb120312e11a3b980d5c222fcaf2
ershadul1 <ershadul.rayhan@gmail.com>
2020-10-01 14:30:05 +0600

    Transfer scopes from event.rb to application_record.rb

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,6 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  scope :past, -> { where('date < ?', Date.today) }
+  scope :upcoming, -> { where('date >= ?', Date.today) }
 end
