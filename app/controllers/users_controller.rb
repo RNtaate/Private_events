a1110b221e82703565aa5f0daabba676d2050303
ershadul1 <ershadul.rayhan@gmail.com>
2020-10-01 14:32:26 +0600

    Use scopes to represent queries in view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,8 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @created_events = @user.created_events
+    @past = @user.attended_events.past
+    @upcoming = @user.attended_events.upcoming
   end
 
   def create
